0dd21a521d83cddcf81fff08b1c736c08304949a
ParimSsme <parima.ssme@gmail.com>
2023-08-15 20:43:08 +0430

    add product model and screen

---
+++ lib/screens/products_overview_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ProductsOverviewScreen extends StatelessWidget {
+  const ProductsOverviewScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

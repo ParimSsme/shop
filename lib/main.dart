816e952342e1874f8f4d097ef19ea5c76c4e91b2
ParimSsme <parima.ssme@gmail.com>
2023-08-16 08:59:36 +0430

    load products in main page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shop/screens/products_overview_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,23 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('MyShop'),
-      ),
-      body: const Center(
-        child: Text('Let\'s build a shop!'),
-      ),
+      home: ProductsOverviewScreen(),
     );
   }
 }
--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -1,10 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:shop/widgets/product_item.dart';
+import '../models/product.dart';
 
 class ProductsOverviewScreen extends StatelessWidget {
-  const ProductsOverviewScreen({super.key});
+  ProductsOverviewScreen({super.key});
+
+  final List<Product> loadedProducts = [
+    Product(
+        id: 'p1',
+        title: 'Red Shirt',
+        description: 'A red shirt - it is pretty red!',
+        price: 29.99,
+        imageUrl:
+            'https://cdn.pixabay.com/photo/2016/10/02/22/17/red-t-shirt-1710578')
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('MyShop'),
+      ),
+      body: GridView.builder(
+        padding: const EdgeInsets.all(10.0),
+        itemCount: loadedProducts.length,
+        itemBuilder: (ctx, i) => ProductItem(
+            loadedProducts[i].id,
+            loadedProducts[i].title,
+            loadedProducts[i].imageUrl
+        ),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10),
+      ),
+    );
   }
 }

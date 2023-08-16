86a49fb158b2c9921e763f890796ff947d9972f4
ParimSsme <parima.ssme@gmail.com>
2023-08-16 09:44:58 +0430

    styling main screen items

--- lib/main.dart
+++ lib/main.dart
@@ -11,10 +11,14 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp(  
       title: 'MyShop',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        colorScheme: ThemeData().colorScheme.copyWith(
+          secondary: Colors.deepOrange
+        ),
+        fontFamily: 'Lato',
       ),
       home: ProductsOverviewScreen(),
     );
--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -9,25 +9,30 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridTile(
-      footer: GridTileBar(
-        backgroundColor: Colors.black54,
-        leading: IconButton(
-          icon: const Icon(Icons.favorite),
-          onPressed: () {},
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        footer: GridTileBar(
+          backgroundColor: Colors.black87 ,
+          leading: IconButton(
+            icon: const Icon(Icons.favorite),
+            color: Theme.of(context).colorScheme.secondary,
+            onPressed: () {},
+          ),
+          title: Text(
+            title,
+            textAlign: TextAlign.center,
+          ),
+          trailing: IconButton(
+            icon: const Icon(Icons.shopping_cart),
+            color: Theme.of(context).colorScheme.secondary,
+            onPressed: () {},
+          ),
         ),
-        title: Text(
-          title,
-          textAlign: TextAlign.center,
+        child: Image.network(
+          imageUrl,
+          fit: BoxFit.cover,
         ),
-        trailing: IconButton(
-          icon: const Icon(Icons.shopping_cart),
-          onPressed: () {},
-        ),
-      ),
-      child: Image.network(
-        imageUrl,
-        fit: BoxFit.cover,
       ),
     );
   }

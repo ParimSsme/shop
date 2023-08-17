e789c4f0559e1c090a23b330d0ec463706c6dcef
ParimSsme <parima.ssme@gmail.com>
2023-08-17 19:50:33 +0430

    get Product in product_detail_screen.dart from provider

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/providers/products.dart';
 
 class ProductDetailScreen extends StatelessWidget {
   // final String title;
@@ -8,11 +10,15 @@ class ProductDetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
-    final productId = ModalRoute.of(context)?.settings.arguments as String; // is the id!
+    final productId =
+        ModalRoute.of(context)?.settings.arguments as String; // is the id!
+    final loadedProduct =
+        Provider.of<Products>(context, listen: false).findById(productId);
 
     return Scaffold(
-      appBar: AppBar(title: Text('title'),),
+      appBar: AppBar(
+        title: Text(loadedProduct.title),
+      ),
     );
   }
 }

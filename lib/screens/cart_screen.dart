d55a745ef461b8f292d2b1c887851d71d1a32d09
ParimSsme <parima.ssme@gmail.com>
2023-08-18 19:13:36 +0430

    displaying list of cart items

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
-import '../providers/cart.dart';
+import '../providers/cart.dart' show Cart;
+import '../widgets/cart_item.dart';
 
 class CartScreen extends StatelessWidget {
   const CartScreen({super.key});
@@ -30,12 +30,36 @@ class CartScreen extends StatelessWidget {
                   ),
                   const Spacer(),
                   Chip(
-                    label: Text('\$${cart.totalAmount}', style: TextStyle(color: Theme.of(context).primaryTextTheme.titleLarge?.color),),
+                    label: Text(
+                      '\$${cart.totalAmount}',
+                      style: TextStyle(
+                          color: Theme.of(context)
+                              .primaryTextTheme
+                              .titleLarge
+                              ?.color),
+                    ),
                     backgroundColor: Theme.of(context).colorScheme.primary,
                   ),
-                  TextButton(onPressed: (){}, child: Text('ORDER NOW', style: TextStyle(color: Theme.of(context).colorScheme.primary),))
+                  TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        'ORDER NOW',
+                        style: TextStyle(
+                            color: Theme.of(context).colorScheme.primary),
+                      ))
                 ],
               ),
+            ),
+          ),
+          SizedBox(height: 10),
+          Expanded(
+            child: ListView.builder(
+              itemCount: cart.items.length,
+              itemBuilder: (ctx, i) => CartItem(
+                  id: cart.items.values.toList()[i].id,
+                  title: cart.items.values.toList()[i].title,
+                  quantity: cart.items.values.toList()[i].quantity,
+                  price: cart.items.values.toList()[i].price),
             ),
           )
         ],
---
+++ lib/widgets/cart_item.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class CartItem extends StatelessWidget {
+  final String id;
+  final double price;
+  final int quantity;
+  final String title;
+
+  const CartItem(
+      {super.key,
+      required this.id,
+      required this.price,
+      required this.quantity,
+      required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.symmetric(
+        horizontal: 15,
+        vertical: 4,
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(8),
+        child: ListTile(
+          leading: CircleAvatar(
+            child: Padding(
+              padding: const EdgeInsets.all(5),
+              child: FittedBox(child: Text('\$$price')),
+            ),
+          ),
+          title: Text(title),
+          subtitle: Text('Total: \$${(price * quantity)}'),
+          trailing: Text('$quantity x'),
+        ),
+      ),
+    );
+  }
+}

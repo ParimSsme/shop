aee76d511b72609ca0087e91d558fe39f60f7a0f
ParimSsme <parima.ssme@gmail.com>
2023-09-09 10:33:34 +0430

    add products to firebase database

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -68,24 +68,30 @@ class Products with ChangeNotifier {
   // }
 
   void addProduct(Product product) {
-    const url = 'https://shop-20586-default-rtdb.firebaseio.com/products.json';
-    http.post(Uri.parse(url), body: json.encode({
-      'title': product.title,
-      'description': product.description,
-      'imageUrl': product.imageUrl,
-      'price': product.price,
-      'isFavorite': product.isFavorite,
-    }),);
-    final newProduct = Product(
-      title: product.title,
-      description: product.description,
-      price: product.price,
-      imageUrl: product.imageUrl,
-      id: DateTime.now().toString(),
-    );
-    _items.add(newProduct);
-    // _items.insert(0, newProduct); // at the start of the list
-    notifyListeners();
+    const url = 'https://flutter-update.firebaseio.com/products.json';
+    http
+        .post(
+      Uri.parse(url),
+      body: json.encode({
+        'title': product.title,
+        'description': product.description,
+        'imageUrl': product.imageUrl,
+        'price': product.price,
+        'isFavorite': product.isFavorite,
+      }),
+    )
+        .then((response) {
+      final newProduct = Product(
+        title: product.title,
+        description: product.description,
+        price: product.price,
+        imageUrl: product.imageUrl,
+        id: json.decode(response.body)['name'],
+      );
+      _items.add(newProduct);
+      // _items.insert(0, newProduct); // at the start of the list
+      notifyListeners();
+    });
   }
 
   void updateProduct(String id, Product newProduct) {

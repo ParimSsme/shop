5d13f396ae655475f57872f010cd7e50df1d1b38
ParimSsme <parima.ssme@gmail.com>
2023-09-16 23:16:46 +0430

    update the favorite status in firebase

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/cupertino.dart';
+import 'package:http/http.dart' as http;
 
 class Product with ChangeNotifier {
   final String id;
@@ -17,8 +20,26 @@ class Product with ChangeNotifier {
     this.isFavorite = false
 });
 
-  void toggleFavoriteStatus() {
+  void _setFavValue(bool newValue) {
+    isFavorite = newValue;
+    notifyListeners();
+  }
+
+  void toggleFavoriteStatus() async {
+    final oldStatus = isFavorite;
     isFavorite = !isFavorite;
     notifyListeners();
+    final url = 'https://shop-20586-default-rtdb.firebaseio.com/products/$id.json';
+    try {
+      final response = await http.patch(Uri.parse(url), body: json.encode({
+        'isFavorite': isFavorite
+      }));
+      if (response.statusCode >= 400) {
+        _setFavValue(oldStatus);
+      }
+    } catch(error) {
+      _setFavValue(oldStatus);
+    }
+
   }
 }

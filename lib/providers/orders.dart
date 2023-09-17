d09fe96f3efe377fa20de05200e7f12ec3d0c49e
ParimSsme <parima.ssme@gmail.com>
2023-09-17 08:04:03 +0430

    store orders in the web

--- lib/providers/orders.dart
+++ lib/providers/orders.dart
@@ -1,5 +1,7 @@
-import 'package:flutter/cupertino.dart';
+import 'dart:convert';
 
+import 'package:flutter/cupertino.dart';
+import 'package:http/http.dart' as http;
 import 'cart.dart';
 
 
@@ -23,11 +25,24 @@ class Orders with ChangeNotifier {
     return [..._orders];
   }
 
-  void addOrder(List<CartItem> cartProducts, double total) {
+  Future<void> addOrder(List<CartItem> cartProducts, double total) async {
+    const url = 'https://shop-20586-default-rtdb.firebaseio.com/orders.json';
+    final timestamp = DateTime.now();
+    final response = await http.post(Uri.parse(url), body: json.encode({
+      'amount': total,
+      'dateTime': timestamp.toIso8601String(),
+      'products': cartProducts.map((cp) => {
+        'id': cp.id,
+        'title': cp.title,
+        'quantity': cp.quantity,
+        'price': cp.price
+      })
+      .toList()
+    }));
     _orders.insert(
       0,
       OrderItem(
-        id: DateTime.now().toString(),
+        id: json.decode(response.body)['name'],
         amount: total,
         products: cartProducts,
         dateTime: DateTime.now(),
--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -4,11 +4,16 @@ import '../providers/cart.dart' show Cart;
 import '../providers/orders.dart';
 import '../widgets/cart_widget.dart';
 
-class CartScreen extends StatelessWidget {
+class CartScreen extends StatefulWidget {
   const CartScreen({super.key});
 
   static const routeName = '/cart';
 
+  @override
+  State<CartScreen> createState() => _CartScreenState();
+}
+
+class _CartScreenState extends State<CartScreen> {
   @override
   Widget build(BuildContext context) {
     final cart = Provider.of<Cart>(context);
@@ -41,19 +46,7 @@ class CartScreen extends StatelessWidget {
                     ),
                     backgroundColor: Theme.of(context).colorScheme.primary,
                   ),
-                  TextButton(
-                      onPressed: () {
-                        Provider.of<Orders>(context, listen: false).addOrder(
-                          cart.items.values.toList(),
-                          cart.totalAmount,
-                        );
-                        cart.clear();
-                      },
-                      child: Text(
-                        'ORDER NOW',
-                        style: TextStyle(
-                            color: Theme.of(context).colorScheme.primary),
-                      ))
+                  OrderButton(cart: cart)
                 ],
               ),
             ),
@@ -73,5 +66,44 @@ class CartScreen extends StatelessWidget {
         ],
       ),
     );
+  }
+}
+
+class OrderButton extends StatefulWidget {
+  const OrderButton({
+    super.key,
+    required this.cart,
+  });
+
+  final Cart cart;
+
+  @override
+  State<OrderButton> createState() => _OrderButtonState();
+}
+
+class _OrderButtonState extends State<OrderButton> {
+  var _isLoading = false;
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+        onPressed: (widget.cart.totalAmount <= 0 || _isLoading)
+            ? null
+            : () async {
+                setState(() {
+                  _isLoading = true;
+                });
+                await Provider.of<Orders>(context, listen: false).addOrder(
+                  widget.cart.items.values.toList(),
+                  widget.cart.totalAmount,
+                );
+                setState(() {
+                  _isLoading = false;
+                });
+                widget.cart.clear();
+              },
+        child: _isLoading ? const CircularProgressIndicator() : Text(
+          'ORDER NOW',
+          style: TextStyle(color: Theme.of(context).colorScheme.primary),
+        ));
   }
 }

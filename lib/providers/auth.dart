87ef8d1dd8d5212f54a6738cef5592b6d4c8b450
ParimSsme <parima.ssme@gmail.com>
2023-10-04 21:53:03 +0430

    login in user

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -4,13 +4,14 @@ import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
 
 class Auth with ChangeNotifier {
-  late final String _token;
-  late final DateTime _expiryDate;
-  late final String _userId;
+  late String _token;
+  late DateTime _expiryDate;
+  late String _userId;
 
-  Future<void> signup(String email, String password) async {
-    const url =
-        'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyDqmNxCM4FppQlaL_baWYhnqSrGmoanh4g';
+  Future<void> _authenticate(
+      String email, String password, String urlSegment) async {
+    final url =
+        'https://www.googleapis.com/identitytoolkit/v3/relyingparty/$urlSegment?key=AIzaSyDqmNxCM4FppQlaL_baWYhnqSrGmoanh4g';
     final response = await http.post(
       Uri.parse(url),
       body: json.encode(
@@ -23,4 +24,13 @@ class Auth with ChangeNotifier {
     );
     print(json.decode(response.body));
   }
+
+  Future<void> signup(String email, String password) async {
+    return _authenticate(email, password, 'signupNewUser');
+  }
+
+  Future<void> login(String email, String password) async {
+    return _authenticate(email, password, 'verifyPassword');
+  }
 }
+
